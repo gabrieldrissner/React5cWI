6945536f258187a90ddc3f7de3bdff5bc4b51db9
Gabriel <gabriel.drissner@student.htldornbirn.at>
2025-03-24 14:14:41 +0100

    added name

--- 5cwi_next_dem/src/app/components/cars/Card.tsx
+++ 5cwi_next_dem/src/app/components/cars/Card.tsx
@@ -11,7 +11,7 @@ export default function Card({ car }: Props) {
             className={`shadow p-4 hover:cursor-pointer border-4 rounded-4xl ${isActive ? "bg-amber-200" : "bg-blue-200"}`}
             onClick={() => setIsActive(!isActive)}
         >
-            
+            <h2 className="font-bold text-3xl">{car.name}</h2>
             <div>{car.color}</div>
             <div>{car.image}</div>
             <div>{isActive ? "active" : "inactive"}</div>
