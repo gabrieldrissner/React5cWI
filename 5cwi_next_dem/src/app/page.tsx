01e9920d3c0babc19ba330641ba8001e49e00bbf
NateGantioler <nate.gantioler@gmail.com>
2025-03-24 14:25:44 +0100

    Nate isch da

--- 5cwi_next_dem/src/app/page.tsx
+++ 5cwi_next_dem/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        Das ist der Inhalt
+        Das ist der Inhalt, hier ist Nate
       </div>
     </div>
   );
