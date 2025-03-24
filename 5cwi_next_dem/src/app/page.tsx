d198e7a2172b8b3f95819aae16e4a54084fd2994
Gabriel <gabriel.drissner@student.htldornbirn.at>
2025-03-24 13:36:37 +0100

    now it should work

--- 5cwi_next_dem/src/app/page.tsx
+++ 5cwi_next_dem/src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import Menu from "./components/ui/menu";
-import Card from "./components/cars/Card";
+import React from "react";
 
 export default function Home() {
   return (
@@ -8,7 +6,6 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         Das ist der Inhalt
       </div>
-      
     </div>
   );
 }
