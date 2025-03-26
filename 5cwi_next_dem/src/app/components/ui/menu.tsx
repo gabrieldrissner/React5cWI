8362705f8a35911922e6da033ce42e8769a7a958
Gabriel <gabriel.drissner@student.htldornbirn.at>
2025-03-26 12:18:22 +0100

    changed leos political fragile perspective

--- 5cwi_next_dem/src/app/components/ui/menu.tsx
+++ 5cwi_next_dem/src/app/components/ui/menu.tsx
@@ -43,7 +43,7 @@ export default function Menu() {
             href="/gayb"
             className="px-4 py-2 rounded-md transition duration-300 hover:bg-gray-700"
           >
-            gayb
+            gabe
           </Link>
         </li>
       </ul>
