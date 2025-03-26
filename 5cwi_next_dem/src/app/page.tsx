db32b5ed6b3a8685c8676fa025b1be394481c4a0
Gabriel <gabriel.drissner@student.htldornbirn.at>
2025-03-26 12:11:47 +0100

    changed gabriel feature

--- 5cwi_next_dem/src/app/page.tsx
+++ 5cwi_next_dem/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
        
         <div className="text-4xl font-bold bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text animate-pulse transform hover:scale-110 transition-transform duration-300 p-4 border-4 border-purple-500 rounded-xl shadow-lg hover:shadow-xl"> Nate hat diese Zeile geschrieben</div>
-        <div className="text-4xl font-bold bg-gradient-to-r from-blue-500 to-green-400 text-transparent bg-clip-text animate-bounce transform hover:rotate-2 hover:scale-105 transition-transform duration-500 p-4 border-4 border-blue-400 rounded-xl shadow-lg hover:shadow-2xl">
+        <div className="text-8xl font-bold bg-gradient-to-r from-blue-500 to-green-400 text-transparent bg-clip-text animate-bounce transform hover:rotate-4 hover:scale-130 transition-transform duration-500 p-12 border-8 border-blue-400 rounded-8xl shadow-lg hover:shadow-8xl">
     Gabriel hat den Code gemerged
 </div>
 
