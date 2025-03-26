83c0717349411e1f2790a60b9c515e1332c8ad4e
Gabriel <gabriel.drissner@student.htldornbirn.at>
2025-03-26 12:20:58 +0100

    changed to sam

--- 5cwi_next_dem/src/app/gayb/page.tsx
+++ 5cwi_next_dem/src/app/gayb/page.tsx
@@ -44,7 +44,7 @@ export default function Trucks() {
                 onMouseEnter={(e) => e.currentTarget.style.transform = "scale(1.1)"}
                 onMouseLeave={(e) => e.currentTarget.style.transform = "scale(1)"}
             >
-                Gabe stinkt
+                Sam stinkt
             </button>
             <style>{`
                 .screen-flash {
